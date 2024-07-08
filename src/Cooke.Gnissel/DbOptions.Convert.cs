090128e5be3d7ff78358026fcd28c9a26ec9d338
Henrik Cooke <henrik@cooke.se>
2024-07-08 09:05:54 +0200

    Extracted convert methods to partial DbOptions

---
+++ src/Cooke.Gnissel/DbOptions.Convert.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection;
+
+namespace Cooke.Gnissel;
+
+public partial class DbOptions
+{
+    private ConcreteDbConverter<T>? GetConverter<T>()
+    {
+        return (ConcreteDbConverter<T>?)GetConverter(typeof(T));
+    }
+
+    public ConcreteDbConverter? GetConverter(Type type)
+    {
+        if (_concreteConverters.TryGetValue(type, out var converter))
+        {
+            return converter;
+        }
+
+        var converterAttribute = type.GetCustomAttribute<DbConverterAttribute>();
+        if (converterAttribute != null)
+        {
+            return GetConverterFromAttribute(type, converterAttribute);
+        }
+
+        var factory = _converterFactories.FirstOrDefault(x => x.CanCreateFor(type));
+        if (factory != null)
+        {
+            var newConverter = factory.Create(type);
+            _concreteConverters.TryAdd(type, newConverter);
+            return newConverter;
+        }
+
+        return null;
+    }
+
+    private static Type? GetTypeToConvertFor(Type? type)
+    {
+        while (true)
+        {
+            if (type is null)
+            {
+                return null;
+            }
+
+            if (
+                type.IsGenericType
+                && type.GetGenericTypeDefinition() == typeof(ConcreteDbConverter<>)
+            )
+            {
+                return type.GetGenericArguments().Single();
+            }
+
+            type = type.BaseType;
+        }
+    }
+
+    private ConcreteDbConverter GetConverterFromAttribute(
+        Type type,
+        DbConverterAttribute converterAttribute
+    )
+    {
+        ConcreteDbConverter? converter;
+        if (converterAttribute.ConverterType.IsAssignableTo(typeof(ConcreteDbConverterFactory)))
+        {
+            var factory =
+                (ConcreteDbConverterFactory?)
+                    Activator.CreateInstance(converterAttribute.ConverterType)
+                ?? throw new InvalidOperationException(
+                    $"Could not create an instance of converter factory of type {converterAttribute.ConverterType}"
+                );
+            if (!factory.CanCreateFor(type))
+            {
+                throw new InvalidOperationException(
+                    $"Factory of type {factory.GetType()} cannot create converter for type {type}"
+                );
+            }
+
+            converter = factory.Create(type);
+            _concreteConverters.TryAdd(type, converter);
+            return converter;
+        }
+
+        if (
+            converterAttribute.ConverterType.IsAssignableTo(
+                typeof(ConcreteDbConverter<>).MakeGenericType(type)
+            )
+        )
+        {
+            converter =
+                (ConcreteDbConverter?)Activator.CreateInstance(converterAttribute.ConverterType)
+                ?? throw new InvalidOperationException(
+                    $"Could not create an instance of converter of type {converterAttribute.ConverterType}"
+                );
+            _concreteConverters.TryAdd(type, converter);
+            return converter;
+        }
+
+        throw new InvalidOperationException(
+            $"Converter of type {converterAttribute.ConverterType} is not a valid converter for type {type}"
+        );
+    }
+}
--- src/Cooke.Gnissel/DbOptions.cs
+++ src/Cooke.Gnissel/DbOptions.cs
@@ -3,7 +3,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Data.Common;
-using System.Reflection;
 using Cooke.Gnissel.Services;
 using Cooke.Gnissel.Services.Implementations;
 
@@ -11,7 +10,7 @@ using Cooke.Gnissel.Services.Implementations;
 
 namespace Cooke.Gnissel;
 
-public class DbOptions
+public partial class DbOptions
 {
     private readonly ConcurrentDictionary<Type, object> _readers = new();
     private readonly ConcurrentDictionary<Type, ConcreteDbConverter> _concreteConverters;
@@ -104,100 +103,4 @@ public class DbOptions
         (ObjectReader<T>)_readers.GetOrAdd(typeof(T), _ => _objectReaderFactory.Create<T>(this));
 
     public bool IsDbMapped(Type type) => GetConverter(type) != null || DbAdapter.IsDbMapped(type);
-
-    private ConcreteDbConverter<T>? GetConverter<T>()
-    {
-        return (ConcreteDbConverter<T>?)GetConverter(typeof(T));
-    }
-
-    public ConcreteDbConverter? GetConverter(Type type)
-    {
-        if (_concreteConverters.TryGetValue(type, out var converter))
-        {
-            return converter;
-        }
-
-        var converterAttribute = type.GetCustomAttribute<DbConverterAttribute>();
-        if (converterAttribute != null)
-        {
-            return GetConverterFromAttribute(type, converterAttribute);
-        }
-
-        var factory = _converterFactories.FirstOrDefault(x => x.CanCreateFor(type));
-        if (factory != null)
-        {
-            var newConverter = factory.Create(type);
-            _concreteConverters.TryAdd(type, newConverter);
-            return newConverter;
-        }
-
-        return null;
-    }
-
-    private static Type? GetTypeToConvertFor(Type? type)
-    {
-        while (true)
-        {
-            if (type is null)
-            {
-                return null;
-            }
-
-            if (
-                type.IsGenericType
-                && type.GetGenericTypeDefinition() == typeof(ConcreteDbConverter<>)
-            )
-            {
-                return type.GetGenericArguments().Single();
-            }
-
-            type = type.BaseType;
-        }
-    }
-
-    private ConcreteDbConverter GetConverterFromAttribute(
-        Type type,
-        DbConverterAttribute converterAttribute
-    )
-    {
-        ConcreteDbConverter? converter;
-        if (converterAttribute.ConverterType.IsAssignableTo(typeof(ConcreteDbConverterFactory)))
-        {
-            var factory =
-                (ConcreteDbConverterFactory?)
-                    Activator.CreateInstance(converterAttribute.ConverterType)
-                ?? throw new InvalidOperationException(
-                    $"Could not create an instance of converter factory of type {converterAttribute.ConverterType}"
-                );
-            if (!factory.CanCreateFor(type))
-            {
-                throw new InvalidOperationException(
-                    $"Factory of type {factory.GetType()} cannot create converter for type {type}"
-                );
-            }
-
-            converter = factory.Create(type);
-            _concreteConverters.TryAdd(type, converter);
-            return converter;
-        }
-
-        if (
-            converterAttribute.ConverterType.IsAssignableTo(
-                typeof(ConcreteDbConverter<>).MakeGenericType(type)
-            )
-        )
-        {
-            converter =
-                (ConcreteDbConverter?)Activator.CreateInstance(converterAttribute.ConverterType)
-                ?? throw new InvalidOperationException(
-                    $"Could not create an instance of converter of type {converterAttribute.ConverterType}"
-                );
-            _concreteConverters.TryAdd(type, converter);
-            return converter;
-        }
-
-        throw new InvalidOperationException(
-            $"Converter of type {converterAttribute.ConverterType} is not a valid converter for type {type}"
-        );
-    }
 }
