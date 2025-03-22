eff9be094e46612addbb592710d20649f3293446
Henrik Cooke <henrik@cooke.se>
2025-03-22 08:58:37 +0100

    Read add

---
+++ src/Cooke.Gnissel/Services/Implementations/DictionaryObjectWriterProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Immutable;
+
+namespace Cooke.Gnissel.Services.Implementations;
+
+public class DictionaryObjectWriterProvider(IImmutableDictionary<Type, IObjectWriter> writers)
+    : IObjectWriterProvider
+{
+    public ObjectWriter<T> Get<T>() =>
+        writers.TryGetValue(typeof(T), out var writer)
+            ? (ObjectWriter<T>)writer
+            : DefaultObjectWriter<T>.Instance;
+
+    private static class DefaultObjectWriter<T>
+    {
+        public static readonly ObjectWriter<T> Instance = new(
+            (value, parameterWriter) => parameterWriter.Write(value)
+        );
+    }
+
+    public static DictionaryObjectWriterProvider From(IEnumerable<IObjectWriter> writers)
+    {
+        var builder = ImmutableDictionary.CreateBuilder<Type, IObjectWriter>();
+        foreach (var writer in writers)
+        {
+            builder.Add(writer.ObjectType, writer);
+        }
+
+        return new DictionaryObjectWriterProvider(builder.ToImmutable());
+    }
+}
